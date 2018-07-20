d7f3af6a09b0f2f3106c00590ce39aeb7f267331
ARYUNAP <AMI619847@GMAIL.com>
2018-07-20 10:06:50 +0200

    still working on styles and fraud2

--- client/src/components/tickets/TicketDetails.js
+++ client/src/components/tickets/TicketDetails.js
@@ -5,72 +5,74 @@ import {fetchAllComments, createComment} from '../../actions/comments';
 import TicketForm from './TicketForm';
 import CommentForm from '../comments/CommentForm';
 import {Link} from 'react-router-dom';
-import FraudRisk from '../FraudRisk';
 
 class TicketDetails extends PureComponent {  
   state = {
     edit: false
   }
 
-  // authorRisk() {
-  //   const userIdTickets = this.props.tickets.map(ticket => {
-  //     ticket.user.id
-  //   })
-  //   const numTicketsByUser = userIdTickets.filter(creator => {
-  //     creator = this.props.ticket.user.id
-  //   }).length
+  authorRisk() {
+    const userIdTickets = this.props.tickets.map(ticket => {
+      return ticket.user.id
+    })
+    const numTicketsByUser = userIdTickets.filter(creator => {
+      return creator = this.props.ticket.user.id
+    }).length
 
-  //   if (numTicketsByUser === 1) 
-  //     return 10 
-  //     else return 0
-  // }
+    if (numTicketsByUser === 1) 
+      return 10 
+      else return 0
+  }
   
-  // averagePriceRisk() {
-  //   const ticketsPrices = this.props.tickets.map(ticket => ticket.price) 
-  //   const totalTicketsSum = ticketsPrices.reduce((accumulator, currentValue) => {
-  //     accumulator + currentValue
-  //   })                
-  //   const averagePrice = totalTicketsSum/this.props.tickets.length 
+  averagePriceRisk() {
+    const ticketsPrices = this.props.tickets.map(ticket => ticket.price) 
+     //console.log(this.props.tickets)
+    const totalTicketsSum = ticketsPrices.reduce((accumulator, currentValue) => {
+      return (accumulator + currentValue)
+    })                
+    const averagePrice = totalTicketsSum/this.props.tickets.length 
     
-  //   const expensiveRisk = ((averagePrice - this.props.ticket.price)/averagePrice)*100
-  //   const cheapRisk = ((this.props.ticket.price - averagePrice)/averagePrice)*100
+    const expensiveRisk = ((averagePrice - this.props.ticket.price)/averagePrice)*100
+    const cheapRisk = ((this.props.ticket.price - averagePrice)/averagePrice)*100
 
-  //   if (averagePrice>this.props.ticket.price){
-  //     return cheapRisk 
-  //   } else if (cheapRisk<-10){
-  //     return -10
-  //   } else return expensiveRisk    
-  // }
+    if (averagePrice>this.props.ticket.price){
+      return cheapRisk 
+    } else if (cheapRisk<-10){
+      return -10
+    } else return expensiveRisk    
+  }
 
-  // hoursRisk() {
-  //   const timestamp_hours = this.props.ticket.createdAt.slice(11, 13)          
-  //   if (timestamp_hours >= 9 && timestamp_hours <= 17) {
-  //       return -10
-  //   } else return 10
-  // }
+  hoursRisk() {
+    const timestamp_hours = this.props.ticket.createdAt.slice(11, 13)          
+    if (timestamp_hours >= 9 && timestamp_hours <= 17) {
+        return -10
+    } else return 10
+  }
 
-  // commentsRisk(){
-  //   if (this.props.comments.length > 3) {
-  //       return 5
-  //   } else return 0
-  // }
+  commentsRisk(){
+    if (this.props.comments.length > 3) {
+        return 5
+    } else return 0
+  }
 
-  // FraudRisk() {
-  //   const total = this.authorRisk() + this.averagePriceRisk() + this.hoursRisk() + this.commentsRisk()
+  FraudRisk() {
+    const total = this.authorRisk() + 
+    // this.averagePriceRisk() + 
+      this.hoursRisk() + this.commentsRisk()
     
-  //   if (total<5) {
-  //       return 5 
-  //   } else if (total>95) {
-  //       return "It is a possible fraud"
-  //   } else return total    
+    if (total<5) {
+        return 5 
+    } else if (total>95) {
+        return "It is a possible fraud"
+    } else return total    
     
-  //   // const newTotal = new Number(total)
-  //   // if (newTotal<5) {
-  //   //     return 5 
-  //   // } else if (newTotal>95) {
-  //   //     return "It is a possible fraud"
-  //   // } else return newTotal
-  // } 
+    // const newTotal = new Number(total)
+    // if (newTotal<5) {
+    //     return 5 
+    // } else if (newTotal>95) {
+    //     return "It is a possible fraud"
+    // } else return newTotal
+  } 
     
   createComment = (comment) => {
     comment.ticket = this.props.ticket
@@ -109,8 +111,7 @@ class TicketDetails extends PureComponent {
         { !this.state.edit && 
           <div>
             <h1>{ticket.ticketAuthor}</h1>             
-            {/* <p>{this.FraudRisk()}</p> */}
-            <p>Fraud Risk = <FraudRisk/> %</p> 
+            <p>Fraud Risk = {this.FraudRisk()} %</p>
             <p>{ticket.price} &euro;</p>
             <p>{ticket.description}</p>
          
